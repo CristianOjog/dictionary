d0ca39a8249a5bf2f1087695cd4e20e2c626c39e
CristianOjog <cristianconstantin.ojog@gmail.com>
2023-01-29 14:08:22 +0200

    I modified the code so that I no longer use an 'event' object

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,53 @@
 const clickAdd = document.querySelector('.clickAdd');
 const clickSearch = document.querySelector('.clickSearch');
 const message = document.querySelector('#msg p');
+const clearButton = document.querySelector('.clearButton');
 
-let dictionary = [];
+let dictionary;
 
 function addWord() {
-    event.preventDefault();
     const word = document.getElementById('addword').value;
+    dictionary = JSON.parse(localStorage.getItem('Dictionary') || "[]");
     dictionary.push(word);
-    document.querySelector('form').reset();
+    console.log(dictionary);
     localStorage.setItem('Dictionary', JSON.stringify(dictionary));
+    document.querySelector('form').reset();
 }
 
 clickAdd.addEventListener('click', addWord);
 
 function searchWord() {
-    event.preventDefault();
     const wordSearched = document.getElementById('wsearch').value;
+    dictionary = JSON.parse(localStorage.getItem('Dictionary') || "[]");
     if (dictionary.includes(wordSearched)) {
         message.textContent = `The word ${wordSearched} is in the dictionary.`;
         message.style.background = 'green';
+        localStorage.setItem('message', message.textContent);
+        localStorage.setItem('style', message.style.background);
     } else {
         message.textContent = `The word ${wordSearched} is not in the dictionary.`;
         message.style.background = 'red';
+        localStorage.setItem('message', message.textContent);
+        localStorage.setItem('style', message.style.background);
     }
     wordSearched.value = '';
 }
 
+window.addEventListener('load', () => {
+    const messageContent = localStorage.getItem('message');
+    const messageStyle = localStorage.getItem('style');
+    if (messageContent) {
+        message.textContent = messageContent;
+        message.style.background = messageStyle;
+    }
+});
+
 clickSearch.addEventListener('click', searchWord);
+
+clearButton.addEventListener('click', () => {
+    localStorage.removeItem('Dictionary');
+    localStorage.removeItem('message');
+    localStorage.removeItem('style');
+    message.textContent = '';
+    message.style.display = 'none';
+});
